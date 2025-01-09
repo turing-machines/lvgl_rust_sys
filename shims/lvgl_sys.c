3bd45bf0ad69bb65fb256722b67c01895e784016
Sven Rademakers <sven.rademakers@gmail.com>
2025-01-09 08:15:55 +0000

    workaround: update old conversion func of lv_color_t

--- shims/lvgl_sys.c
+++ shims/lvgl_sys.c
@@ -1,26 +1,13 @@
 #include "lvgl_sys.h"
 
-lv_color_t _LV_COLOR_MAKE(uint8_t r, uint8_t g, uint8_t b)
-{
-    return lv_color_make(r, g, b);
+lv_color_t _LV_COLOR_MAKE(uint8_t r, uint8_t g, uint8_t b) {
+  return lv_color_make(r, g, b);
 }
 
-uint16_t _LV_COLOR_GET_R(lv_color_t color)
-{
-    return LV_COLOR_GET_R(color);
-}
+uint32_t _LV_COLOR_GET_R(lv_color32_t color) { return color.red; }
 
-uint16_t _LV_COLOR_GET_G(lv_color_t color)
-{
-    return LV_COLOR_GET_G(color);
-}
+uint32_t _LV_COLOR_GET_G(lv_color32_t color) { return color.green; }
 
-uint16_t _LV_COLOR_GET_B(lv_color_t color)
-{
-    return LV_COLOR_GET_B(color);
-}
+uint32_t _LV_COLOR_GET_B(lv_color32_t color) { return color.blue; }
 
-uint16_t _LV_COLOR_GET_A(lv_color_t color)
-{
-    return LV_COLOR_GET_A(color);
-}
+uint32_t _LV_COLOR_GET_A(lv_color32_t color) { return color.alpha; }
--- shims/lvgl_sys.h
+++ shims/lvgl_sys.h
@@ -2,17 +2,16 @@
 #define LVGL_API_H
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 #include "../lvgl/lvgl.h"
 
-    lv_color_t _LV_COLOR_MAKE(uint8_t r, uint8_t g, uint8_t b);
-    uint16_t _LV_COLOR_GET_R(lv_color_t color);
-    uint16_t _LV_COLOR_GET_G(lv_color_t color);
-    uint16_t _LV_COLOR_GET_B(lv_color_t color);
-    uint16_t _LV_COLOR_GET_A(lv_color_t color);
+lv_color_t _LV_COLOR_MAKE(uint8_t r, uint8_t g, uint8_t b);
+uint32_t _LV_COLOR_GET_R(lv_color32_t color);
+uint32_t _LV_COLOR_GET_G(lv_color32_t color);
+uint32_t _LV_COLOR_GET_B(lv_color32_t color);
+uint32_t _LV_COLOR_GET_A(lv_color32_t color);
 
 #ifdef __cplusplus
 } /* extern "C" */
